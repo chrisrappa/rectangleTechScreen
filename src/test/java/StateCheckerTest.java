51c737dbaad428f6a2f5c1710c666a0fd7c90587
chrisrappa <rappaproductionsinc@gmail.com>
2021-10-22 08:57:03 -0500

    Full coverage of stateChecker achieved

--- src/test/java/StateCheckerTest.java
+++ src/test/java/StateCheckerTest.java
@@ -1,33 +1,66 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-
-import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class StateCheckerTest {
 
+    StateChecker stateChecker;
+
     @Mock
-    OverLapCheck overLapCheck;
-    AdjacentCheck adjacentCheck;
-    ContainedCheck containedCheck;
-    Rectangle rect1;
-    Rectangle rect2;
+    public static OverLapCheck overLapCheck;
+    public static AdjacentCheck adjacentCheck;
+    public static ContainedCheck containedCheck;
+    public static Rectangle rect1;
+    public static Rectangle rect2;
+
+
+    @BeforeEach
+    public void setUp(){
+        overLapCheck = mock(OverLapCheck.class);
+        adjacentCheck = mock(AdjacentCheck.class);
+        containedCheck = mock(ContainedCheck.class);
+        rect1 = mock(Rectangle.class);
+        rect2 = mock(Rectangle.class);
+        stateChecker = new StateChecker(overLapCheck, containedCheck, adjacentCheck, rect1, rect2);
+
+    }
 
     @Test
     public void shouldReturnIsAdjacent() {
-
         when(adjacentCheck.isAdjacent(rect1, rect2)).thenReturn(true);
-        when(containedCheck.isContained(rect1, rect2)).thenReturn(false);
-        when(overLapCheck.isOverLapping(rect1, rect2)).thenReturn(false);
+        String returnValue = stateChecker.checkState(overLapCheck, containedCheck, adjacentCheck, rect1, rect2);
 
-        StateChecker stateChecker = new StateChecker(overLapCheck, containedCheck, adjacentCheck, rect1, rect2);
         String expected = "isAdjacent";
+        assertEquals(expected, returnValue);
+    }
 
-        assertEquals(expected, stateChecker);
-        
+    @Test
+    public void shouldReturnIsContained() {
+        when(containedCheck.isContained(rect1, rect2)).thenReturn(true);
+        String returnValue = stateChecker.checkState(overLapCheck, containedCheck, adjacentCheck, rect1, rect2);
+
+        String expected = "isContained";
+        assertEquals(expected, returnValue);
+    }
+
+    @Test
+    public void shouldReturnIsOverlapping() {
+        when(overLapCheck.isOverLapping(rect1, rect2)).thenReturn(true);
+        String returnValue = stateChecker.checkState(overLapCheck, containedCheck, adjacentCheck, rect1, rect2);
+
+        String expected = "isOverlapping";
+        assertEquals(expected, returnValue);
+    }
+
+    @Test
+    public void shouldReturnEmptyString() {
+        String returnValue = stateChecker.checkState(overLapCheck, containedCheck, adjacentCheck, rect1, rect2);
+        String expected = "";
+
+        assertEquals(expected, returnValue);
     }
 }
